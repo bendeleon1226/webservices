95b6d1014be5ca134d335a5376b181c40907d4bf
bendeleon1226 <benedicto.gualda.deleon@gmail.com>
2016-08-14 22:40:29 +0800

    chapter 01 changes

---
+++ JavaWebServicesUpAndRunning/src/ch01/team/Team.java
@@ -0,0 +1,34 @@
+package ch01.team;
+
+import java.util.List;
+
+public class Team {
+	private List<Player> players;
+	private String name;
+	
+	public Team(){
+	}
+	
+	public Team(String name, List<Player> players){
+		
+	}
+
+	public List<Player> getPlayers() {
+		return players;
+	}
+
+	public void setPlayers(List<Player> players) {
+		this.players = players;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+
+}
