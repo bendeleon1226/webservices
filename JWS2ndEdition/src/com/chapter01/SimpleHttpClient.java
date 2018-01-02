e8fc2de65e1750d24035e8398e6eecb29a53d560
bendeleon1226 <benedicto.gualda.deleon@gmail.com>
2018-01-02 23:41:24 +0800

    simple http client

---
+++ JWS2ndEdition/src/com/chapter01/SimpleHttpClient.java
@@ -0,0 +1,24 @@
+package com.chapter01;
+
+import java.net.Socket;
+import java.net.URL;
+import java.net.MalformedURLException;
+import java.net.UnknownHostException;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class SimpleHttpClient {
+
+	public static void main(String[] args) {
+		
+		//usage
+		if(args.length < 1) {
+			System.err.println("Usage: SimpleHttpClient <url>");
+		}
+		
+
+	}
+
+}
