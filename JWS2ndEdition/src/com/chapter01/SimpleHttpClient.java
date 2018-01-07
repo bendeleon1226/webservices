b69e24758fd875dff1f3ffcf442cd18135327608
bendeleon1226 <benedicto.gualda.deleon@gmail.com>
2018-01-07 23:46:01 +0800

    learning java web services on 07Jan2018

--- JWS2ndEdition/src/com/chapter01/SimpleHttpClient.java
+++ JWS2ndEdition/src/com/chapter01/SimpleHttpClient.java
@@ -16,9 +16,40 @@ public class SimpleHttpClient {
 		//usage
 		if(args.length < 1) {
 			System.err.println("Usage: SimpleHttpClient <url>");
+			return;
 		}
-		
-
+		try {
+			// Parse the URL.
+			URL url = new URL(args[0]);
+			String host = url.getHost();
+			String path = url.getPath();
+			int port =  url.getPort();
+			if(port < 0)
+				port = 80;
+			
+			//send the request.
+			String request = "GET " + path + " HTTP/1.1\n";
+			request += "host: " + host;
+			request += "\n\n";
+			Socket sock = new Socket(host, port);
+			PrintWriter writer = new PrintWriter(sock.getOutputStream());
+			writer.println(request);
+			writer.flush();
+			
+			// Read and print the response.
+			BufferedReader reader = new BufferedReader(new InputStreamReader(sock.getInputStream()));
+			String next_record = null;
+			while((next_record = reader.readLine()) != null){
+				System.out.println(next_record);
+			}
+			sock.close();
+		} catch (MalformedURLException e) {
+			throw new RuntimeException("Please try again. Bad URL.\n" + e);
+		} catch(UnknownHostException e){
+			throw new RuntimeException("Please try again. Unknown host.\n" + e);
+		} catch(IOException e){
+			throw new RuntimeException("Please try again. Something's wrong.\n" + e);
+		}
 	}
 
 }
